334adc97b5f4f68c35b3a03f05ddc6d8061d6aee
docfr10 <docfr10@yandex.ru>
2023-05-10 11:42:04 +0300

    Added simple alarm

--- app/src/main/java/com/example/lesson_clockapp/presentation/Clock.kt
+++ app/src/main/java/com/example/lesson_clockapp/presentation/Clock.kt
@@ -1,5 +1,8 @@
 package com.example.lesson_clockapp.presentation
 
+import android.app.TimePickerDialog
+import android.content.Context
+import android.widget.Toast
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
@@ -18,21 +21,30 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.wear.compose.material.Button
 import androidx.wear.compose.material.MaterialTheme
 import androidx.wear.compose.material.Scaffold
 import androidx.wear.compose.material.Text
 import kotlinx.coroutines.delay
 import java.text.SimpleDateFormat
+import java.util.Calendar
 import java.util.Date
 import java.util.Locale
 
 @Composable
-fun Clock() {
+fun Clock(context: Context) {
     val time = remember { mutableStateOf(Date()) }
+    val alarmTime = remember { mutableStateOf(Date()) }
 
     LaunchedEffect(Unit) {
         while (true) {
             time.value = Date()
+
+            if (alarmTime.value.hours == time.value.hours &&
+                alarmTime.value.minutes == time.value.minutes
+            ) {
+                Toast.makeText(context, "Alarm", Toast.LENGTH_LONG).show()
+            }
             delay(1000)
         }
     }
@@ -67,6 +79,27 @@ fun Clock() {
                     textAlign = TextAlign.Center,
                     color = Color.White
                 )
+
+                Button(
+                    onClick = {
+                        // Open time picker dialog and update alarmTime
+                        TimePickerDialog(
+                            context,
+                            { _, hourOfDay, minute ->
+                                val calendar = Calendar.getInstance()
+                                calendar.set(Calendar.HOUR_OF_DAY, hourOfDay)
+                                calendar.set(Calendar.MINUTE, minute)
+                                alarmTime.value = calendar.time
+                            },
+                            alarmTime.value.hours,
+                            alarmTime.value.minutes,
+                            false
+                        ).show()
+                    },
+                    modifier = Modifier.align(Alignment.BottomEnd)
+                ) {
+                    Text(text = "Set alarm")
+                }
             }
         }
     }
--- app/src/main/java/com/example/lesson_clockapp/presentation/MainActivity.kt
+++ app/src/main/java/com/example/lesson_clockapp/presentation/MainActivity.kt
@@ -3,12 +3,14 @@ package com.example.lesson_clockapp.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.ui.platform.LocalContext
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Clock()
+            val context = LocalContext.current
+            Clock(context = context)
         }
     }
 }
